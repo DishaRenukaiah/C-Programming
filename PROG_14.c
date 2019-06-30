04ccdc722fc2341f5e6f092d6e60b67d8f31a661
Rekha S <rekhas@Rekhas-MacBook-Air.local>
2019-07-01 01:50:59 +0530

    updated program 14

---
+++ PROG_14.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()	{
+
+int a=10 , b=30 , t;
+
+	printf("before swapping the value of a&b is a=%d & b=%d\n",a,b);
+
+	t=a;
+	a=b;
+	b=t;
+
+    printf("after swapping a=%d & b=%d\n",a,b);
+    
+return 0;
+
+}
+
