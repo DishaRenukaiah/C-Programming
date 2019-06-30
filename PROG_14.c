4a7ac34231ecc36f6393788d61080dde5373bf43
Disha Renukaiah <disha.renukaiah@gmail.com>
2019-07-01 02:02:54 +0530

    finished program 14

--- PROG_14.c
+++ PROG_14.c
@@ -1,17 +1,43 @@
 #include <stdio.h>
-int main()	{
+#include <stdlib.h>
+int main() 
+{
+	int a[50],n,i,temp;
+	int *p1 , *p2;
 
-int a=10 , b=30 , t;
+	p1 = p2 = a;
 
-	printf("before swapping the value of a&b is a=%d & b=%d\n",a,b);
+	printf("\n enter the number of elements:");
+	scanf("%d",&n);
+	printf("\n enter the elements:\n");
 
-	t=a;
-	a=b;
-	b=t;
+	for(i=0; i<n; ++i)
+	scanf("%d",&a[i]);
+	for(i=0; i<n; ++i)
 
-    printf("after swapping a=%d & b=%d\n",a,b);
-    
-return 0;
+	{
+
+	if(a[i]<*p1)
+	p1= &a[i];
+	if(a[i]>*p2)
+	p2= &a[i];
+
+	}
+
+	printf("\n array elements before swapping the largest and smallest elements\n");
+	for(i=0; i<n; i++)
+	printf("%d\t",a[i]);
+	printf("\n\n smallest element in an array =%d\n",*p1);
+	printf("\n largest element in an array =%d\n",*p2);
+
+	temp= *p1;
+	*p1 =*p2;
+	*p2 =temp;
+
+	printf("\n array elements after swapping the largest and smallest elements \n");
+	for(i=0; i<n; i++)
+	printf("%d\t", a[i]);
+
+	return 0;
 
 }
-
